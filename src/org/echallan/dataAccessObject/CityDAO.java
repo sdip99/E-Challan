6839388ce47760858c923e0d4b59d0bd30c12a0d
Harsh Panchal <panchal.harsh18@gmail.com>
2014-12-26 10:37:06 +0530

    CityDAO: Implement search functionality
    Signed-off-by: Harsh Panchal <panchal.harsh18@gmail.com>

--- src/org/echallan/dataAccessObject/CityDAO.java
+++ src/org/echallan/dataAccessObject/CityDAO.java
@@ -7,10 +7,31 @@
  *******************************************************************************/
 package org.echallan.dataAccessObject;
 
+import java.util.List;
+
 import org.echallan.valueObject.City;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 
 public class CityDAO extends GenericDAO {
 	public City search(City city) {
-		return null;
+		SessionFactory factory = null;
+		Session session;
+		City rCity = null;
+		try {
+			factory = new Configuration().configure().buildSessionFactory();
+			session = factory.openSession();
+			Transaction transaction = session.beginTransaction();
+			Query query = session.createQuery("from City where name = '" + city.getName() + "'");
+			transaction.commit();
+			List<City> ret = query.list();
+			if(ret != null && ret.size() > 0)
+				rCity = ret.get(0);		// City name has unique constraint so there must be only one result
+			session.close();
+		} catch (Exception ex) { System.err.println(ex); }
+		return rCity;
 	}
 }
