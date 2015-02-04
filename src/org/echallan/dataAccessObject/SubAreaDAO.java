a28f618e314723ab852dc631e4a33990cb9256f3
Harsh Panchal <panchal.harsh18@gmail.com>
2015-02-04 15:37:00 +0530

    SubAreaDAO: Add get subarea by id functionality
    Signed-off-by: Harsh Panchal <panchal.harsh18@gmail.com>

--- src/org/echallan/dataAccessObject/SubAreaDAO.java
+++ src/org/echallan/dataAccessObject/SubAreaDAO.java
@@ -28,4 +28,17 @@ public class SubAreaDAO extends GenericDAO {
 		}
 		return ret;
 	}
+	
+	public SubArea getSubArea(int id) {
+		SubArea ret = null;
+		Session session = getSession();
+		if(session != null) {
+			Transaction transaction = session.beginTransaction();
+			Query query = session.createQuery("from SubArea where subarea_id=" + id);
+			transaction.commit();
+			ret = (SubArea) query.list().get(0);
+			session.close();
+		}
+		return ret;
+	}
 }
